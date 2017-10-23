b23875882fc46dc8fd5a7e0097c22af513594eaa
dylavand3 <dylavand3@student.arteveldehs.be>
2017-10-23 15:46:02 +0200

    Add files via upload

---
+++ opdrachtweek4/Oefening 1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Oefening_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int getal, teller;
+            int aantal = 0;
+            int deler = 1;
+
+
+            Console.Write("Geef een getal in (max 100): ");
+            getal = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            if(getal <= 100)
+            {
+                for (teller = 1; teller <= getal; teller++)
+                {
+                    Console.Write(teller.ToString() + ": ");
+                    aantal = 0;
+
+                    for (deler = 1; deler <= teller; deler++)
+                    {
+                        if (teller % deler == 0)
+                        {
+                            Console.Write(deler.ToString()+ " ");
+
+                            aantal++;
+                        }
+                    }
+
+                    if (aantal == 2) {
+                        Console.Write("Priemgetal");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+            else {
+                Console.WriteLine("Geen priemgetal");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ opdrachtweek4/Oefening 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Opdracht_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intTeller, getal1 = 0, getal2 = 1, getal3;
+
+            Console.Write(getal1.ToString() + " " + getal2.ToString() + " ");
+
+            for (intTeller = 3; intTeller <= 10; intTeller++)
+            {
+                getal3 = getal1 + getal2;
+                getal1 = getal2;
+                getal2 = getal3;
+
+                Console.Write(getal3.ToString() + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
